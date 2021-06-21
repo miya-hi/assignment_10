85900e86e67b2f6581c67644d1ad5e9464b505b8
miyazaki hitomi <hitomikirarin0830m@yahoo.co.jp>
2021-06-21 16:51:43 +0900

    columnはnameとdescriptionのみに変更

--- db/migrate/20210621064042_create_tasks.rb
+++ db/migrate/20210621064042_create_tasks.rb
@@ -3,10 +3,6 @@ class CreateTasks < ActiveRecord::Migration[5.2]
     create_table :tasks do |t|
       t.string :name
       t.text :discription
-      t.integer :priority
-      t.date :deadline
-      t.integer :status
-
       t.timestamps
     end
   end
