05fa05f966f60612024cb6b5c706d26cd80e61ae
miyazaki hitomi <hitomikirarin0830m@yahoo.co.jp>
2021-06-22 13:44:58 +0900

    nameカラムにnull:false設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_21_080000) do
+ActiveRecord::Schema.define(version: 2021_06_22_043954) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
