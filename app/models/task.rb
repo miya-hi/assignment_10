dec4c7b6cf801814f0e7f3165a3c5c23440926fe
miyazaki hitomi <hitomikirarin0830m@yahoo.co.jp>
2021-06-23 11:06:11 +0900

    descriptionにpresence:true

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,4 @@
 class Task < ApplicationRecord
   validates :name, presence: true, length: { minumun:1, maximum: 252 }
-  validates :description, length: { minumun:1, maximum: 1000 }  
+  validates :description, presence: true, length: { minumun:1, maximum: 1000 }  
 end
