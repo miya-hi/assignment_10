5ed54f203d574683a72f498315bd952c0e4127a9
miyazaki hitomi <hitomikirarin0830m@yahoo.co.jp>
2021-06-23 11:04:13 +0900

    テスト作成

---
+++ spec/system/task_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_helper'
+RSpec.describe 'タスク管理機能', type: :system do
+  before do
+    @task = FactoryBot.create(:task, name: 'task_name1',description: 'task_description1')
+  end
+  describe '新規作成機能' do
+    context 'タスクを新規作成した場合' do
+      it '作成したタスクが表示される' do
+        # テストで使用するためのタスクを作成
+        # タスク一覧ページに遷移
+        visit task_path(@task.id)
+        # visitした（遷移した）page（タスク一覧ページ）に「task」という文字列が
+        # have_contentされているか（含まれているか）ということをexpectする（確認・期待する）
+        expect(page).to have_content 'task_name1'
+        expect(page).to have_content 'task_description1'
+        # expectの結果が true ならテスト成功、false なら失敗として結果が出力される
+      end
+    end
+  end
+  describe '一覧表示機能' do
+    context '一覧画面に遷移した場合' do
+      it '作成済みのタスク一覧が表示される' do
+        task = FactoryBot.create(:task, name: 'task_name2',description: 'task_description2')
+        visit tasks_path
+        expect(page).to have_content 'task_name1'
+        expect(page).to have_content 'task_description1'
+        expect(page).to have_content 'task_name2'
+        expect(page).to have_content 'task_description2'
+      end
+    end
+  end
+  describe '詳細表示機能' do
+     context '任意のタスク詳細画面に遷移した場合' do
+       it '該当タスクの内容が表示される' do
+         task = FactoryBot.create(:task, name: 'task_name2',description: 'task_description2')
+         visit task_path(@task.id)
+         expect(page).to have_content 'task_name1'
+         expect(page).to have_content 'task_description1'
+         visit task_path(task.id)
+         expect(page).to have_content 'task_name2'
+         expect(page).to have_content 'task_description2'
+       end
+     end
+  end
+end
