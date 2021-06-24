6a735d1332823c46f864bf58ec2d0c930c789134
miyazaki hitomi <hitomikirarin0830m@yahoo.co.jp>
2021-06-24 16:57:05 +0900

    テスト作成

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -28,6 +28,15 @@ RSpec.describe 'タスク管理機能', type: :system do
         expect(page).to have_content 'task_description2'
       end
     end
+    context 'タスクが作成日時の降順に並んでいる場合' do
+      it '新しいタスクが一番上に表示される' do
+        task = FactoryBot.create(:task, name: 'task_name2',description: 'task_description2')
+        visit tasks_path
+        task_list = all('.task_row')
+        expect(task_list[0]).to have_content 'task_name2'
+        expect(task_list[1]).to have_content 'task_name1'
+      end
+    end
   end
   describe '詳細表示機能' do
      context '任意のタスク詳細画面に遷移した場合' do
