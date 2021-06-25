d552deee0908e4d2a2d71fbad178c7846bce1141
miyazaki hitomi <hitomikirarin0830m@yahoo.co.jp>
2021-06-25 16:01:23 +0900

    status表示欄作成

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,5 @@ class Task < ApplicationRecord
   validates :name, presence: true, length: { minumun:1, maximum: 252 }
   validates :description, presence: true, length: { minumun:1, maximum: 1000 }
   validates :deadline, presence: true
+  validates :status, presence: true
 end
