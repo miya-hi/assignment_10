c455220487f5beac8a5c6b9d9e217348163229f9
miyazaki hitomi <hitomikirarin0830m@yahoo.co.jp>
2021-06-25 16:37:04 +0900

    statusのデータ型をstringに変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_25_061806) do
+ActiveRecord::Schema.define(version: 2021_06_25_073012) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 2021_06_25_061806) do
     t.date "deadline", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "status", null: false
+    t.string "status", null: false
   end
 
 end
