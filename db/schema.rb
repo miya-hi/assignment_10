5943395af20b05e4cd38a2aa6547f6c27d6c7f5e
miyazaki hitomi <hitomikirarin0830m@yahoo.co.jp>
2021-07-01 11:10:30 +0900

    Userモデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_28_073042) do
+ActiveRecord::Schema.define(version: 2021_07_01_020734) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,14 @@ ActiveRecord::Schema.define(version: 2021_06_28_073042) do
     t.string "status", null: false
     t.integer "priority"
     t.index ["name"], name: "index_tasks_on_name"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
