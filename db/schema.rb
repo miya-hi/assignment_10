40c25b01441691fd3837b2359ccf3e88f59e1401
miyazaki hitomi <hitomikirarin0830m@yahoo.co.jp>
2021-07-01 11:35:49 +0900

    tasksテーブルにuser_id追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_01_022733) do
+ActiveRecord::Schema.define(version: 2021_07_01_023135) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,7 +23,9 @@ ActiveRecord::Schema.define(version: 2021_07_01_022733) do
     t.datetime "updated_at", null: false
     t.string "status", null: false
     t.integer "priority"
+    t.bigint "user_id"
     t.index ["name"], name: "index_tasks_on_name"
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -35,4 +37,5 @@ ActiveRecord::Schema.define(version: 2021_07_01_022733) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "tasks", "users"
 end
