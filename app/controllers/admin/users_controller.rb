80f299fcbaef99ebe01f44bbf7144d408ea29547
miyazaki hitomi <hitomikirarin0830m@yahoo.co.jp>
2021-07-02 14:53:49 +0900

    管理者以外はアクセスできない機能追加

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,18 +1,19 @@
 class Admin::UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :exclude_not_admin_user
   def new
     @user = User.new
   end
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to admin_user_url(@user), notice: "ユーザー「#{@user.name}」を登録しました"
+      redirect_to admin_user_path(@user), notice: "ユーザー「#{@user.name}」を登録しました"
     else
       render :new
     end
   end
   def index
-    @users = User.all
+    @users = User.all.includes(:tasks)
   end
   def edit
   end
@@ -35,5 +36,10 @@ class Admin::UsersController < ApplicationController
   end
   def set_user
     @user = User.find(params[:id])
+  end
+  def exclude_not_admin_user
+    if current_user.admin == false
+      redirect_to root_path, notice: 'アクセス権限がありません'
+    end
   end
 end
