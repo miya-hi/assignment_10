89388438fbb2a24dc6b8990740c566aacbd19ed1
miyazaki hitomi <hitomikirarin0830m@yahoo.co.jp>
2021-07-02 14:57:53 +0900

    パスワード使用の制限

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,13 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 255 },
   format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 }, if: lambda{ new_record? || !password.nil? }
   has_many :tasks, dependent: :destroy
 end
+
+# def validates(colomn_name, length:, presence: true, if:)
+#   self.update if if.nil? || if(self)
+# end
+# def sample(a, b, c)
+# end
+# sample(1, 2)
