228e07976a0422c3ff6eae6439b181cd924eb800
miyazaki hitomi <hitomikirarin0830m@yahoo.co.jp>
2021-07-02 10:53:02 +0900

    admin 削除機能作成

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -6,25 +6,30 @@ class Admin::UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to admin_user_url(@user), notice: "ユーザー「#{@user.name}」を登録しました。"
+      redirect_to admin_user_url(@user), notice: "ユーザー「#{@user.name}」を登録しました"
     else
       render :new
     end
   end
-
-
+  def index
+    @users = User.all
+  end
   def edit
   end
   def update
     @user.update(user_params)
-    redirect_to admin_users_path, notice:'権限を更新しました'
+    redirect_to admin_users_path, notice:"ユーザー「#{@user.name}」の権限を更新しました"
   end
   def show
   end
-
-  def index
-    @users = User.all
+  def destroy
+    if @user.destroy
+      redirect_to admin_users_path, notice: "ユーザー「#{@user.name}」を削除しました"
+    else
+      redirect_to admin_users_path, notice: "ユーザー「#{@user.name}」を削除できませんでした"
+    end
   end
+
   def user_params
     params.require(:user).permit(:name, :email, :admin, :password, :password_confirmation)
   end
