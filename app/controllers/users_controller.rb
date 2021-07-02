0eb20575a55561b535579940fb229c58547173aa
miyazaki hitomi <hitomikirarin0830m@yahoo.co.jp>
2021-07-02 21:45:53 +0900

    エラー修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
   skip_before_action :login_required, only: [:new, :create]
+  before_action :redirect_task, only: [:new]
+  before_action :exclude_not_current_user, only: [:show]
+
   def new
     @user = User.new
   end
@@ -14,8 +17,12 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
   private
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+  def exclude_not_current_user
+    redirect_to tasks_path, notice:"アクセスできません" unless current_user&.id == @user&.id
   end
 end
