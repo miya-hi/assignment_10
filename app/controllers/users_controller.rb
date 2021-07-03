d0837b852fb5da276355d2338bb804e2d0a9d682
miyazaki hitomi <hitomikirarin0830m@yahoo.co.jp>
2021-07-03 18:29:39 +0900

    ユーザー登録後マイページに遷移 修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   skip_before_action :login_required, only: [:new, :create]
   before_action :redirect_tasks, only: [:new]
+  before_action :set_user,  only: [:show]
   before_action :exclude_not_current_user, only: [:show]
 
   def new
@@ -9,18 +10,21 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       redirect_to user_path(@user.id)
     else
       render :new
     end
   end
   def show
-    @user = User.find(params[:id])
   end
 
   private
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+  def set_user
+    @user = User.find(params[:id])
   end
   def exclude_not_current_user
     redirect_to tasks_path, notice:"アクセスできません" unless current_user&.id == @user&.id
