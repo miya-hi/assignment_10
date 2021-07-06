e4b36d399a56c426dcd6191edf45ea49b85b9a5e
miyazaki hitomi <hitomikirarin0830m@yahoo.co.jp>
2021-07-06 22:19:32 +0900

    seedデータ変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create(name: "m", email: "h@email.com", password_digest: "123456", admin: true )
+User.create(name: "m", email: "h@email.com", password: "123456", admin: true )
