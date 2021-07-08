385bbe5e47190d08752ae5e2c95dbce3fcc37318
miyazaki hitomi <hitomikirarin0830m@yahoo.co.jp>
2021-07-08 11:56:00 +0900

    task_paramsにlabel追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -80,6 +80,6 @@ class TasksController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def task_params
-    params.require(:task).permit(:name, :description, :deadline, :status, :priority)
+    params.require(:task).permit(:name, :description, :deadline, :status, :priority, :label_ids [])
   end
 end
