45b7850e6d05aa910f4263c67eafdcab0d960b6b
miyazaki hitomi <hitomikirarin0830m@yahoo.co.jp>
2021-07-09 00:00:06 +0900

    タスク、ユーザ、ラベルのシードデータ作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,41 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 User.create(name: "m", email: "h@email.com", password: "123456", admin: true )
+
+10.times do |n|
+  name = Faker::Games::Pokemon.name
+  email = Faker::Internet.email
+  password = "password"
+  User.create!(name: name,
+               email: email,
+               password: password,
+               admin: false
+               )
+end
+10.times do |n|
+  name = "タスク#{n}"
+  description = "内容#{n}"
+  deadline = DateTime.new(2021,5, rand(1..30))
+  status = '着手中'
+  priority = rand(0..2)
+  user_id = rand(0..50)
+  Task.create!(name: name,
+               description: description,
+               deadline: deadline,
+               status: status,
+               priority: priority,
+               user_id: user_id
+               )
+end
+Label.create([
+  { name: '趣味' },
+  { name: '仕事' },
+  { name: '急ぎ'},
+  { name: '学校'},
+  { name: '音楽'},
+  { name: '料理'},
+  { name: '運動'},
+  { name: 'ダイエット'},
+  { name: '息抜き'},
+  { name: '映画'},
+  ])
